423f2e0307ec041dd6dd6f154cbf3f51aa0707c3
Dianash <ddiana.shnaider@gmail.com>
2020-04-15 22:28:35 +0300

    Created GameSceneManager class

--- Dead Earth/Assets/Dead Earth/Scripts/GameSceneManager.cs
+++ Dead Earth/Assets/Dead Earth/Scripts/GameSceneManager.cs
@@ -1,6 +1,44 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class GameSceneManager : MonoBehaviour
 {
-   
+    private static GameSceneManager instance = null;
+
+    public static GameSceneManager Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = (GameSceneManager)FindObjectOfType(typeof(GameSceneManager));
+            return instance;
+        }
+    }
+
+    private Dictionary<int, AIStateMachine> stateMachine = new Dictionary<int, AIStateMachine>();
+
+    /// <summary>
+    /// Stores the passed state machine in the dictionary with the supplied key
+    /// </summary>
+    public void RegisterAIStateMachine(int key, AIStateMachine machine)
+    {
+        if (!stateMachine.ContainsKey(key))
+            stateMachine[key] = machine;
+    }
+
+    /// <summary>
+    /// Returns an AI State Machine reference searched on by the instance ID of an object
+    /// </summary>
+    /// <param name="key">AI State Machine instance ID</param>
+    public AIStateMachine GetAIStateMachine(int key)
+    {
+        AIStateMachine machine = null;
+
+        if (stateMachine.TryGetValue(key, out machine))
+        {
+            return machine;
+        }
+
+        return null;
+    }
 }
