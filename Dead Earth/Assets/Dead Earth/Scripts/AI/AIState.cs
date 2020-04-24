a35d595e2d82ab0b70bdbf355eb58121d248e540
Dianash <ddiana.shnaider@gmail.com>
2020-04-24 22:22:18 +0300

    Added main AIZombieStateMachine behavior properties

--- Dead Earth/Assets/Dead Earth/Scripts/AI/AIState.cs
+++ Dead Earth/Assets/Dead Earth/Scripts/AI/AIState.cs
@@ -11,13 +11,26 @@ public abstract class AIState : MonoBehaviour
     }
 
     public virtual void OnEnterState() { }
+
     public virtual void OnExitState() { }
-    public virtual void OnAnimatorUpdated() { }
+
+    public virtual void OnAnimatorUpdated()
+    {
+        if (stateMachine.UseRootPosition)
+            stateMachine.NavAgent.velocity = stateMachine.Animator.deltaPosition / Time.deltaTime;
+
+        if (stateMachine.UseRootPosition)
+            stateMachine.transform.rotation = stateMachine.Animator.rootRotation;
+    }
+
     public virtual void OnAnimatorIKUpdated() { }
+
     public virtual void OnTriggerEvent(AITriggerEventType eventType, Collider other) { }
+
     public virtual void OnDestinationtReached(bool isReached) { }
 
     public abstract AIStateType GetStateType();
+
     public abstract AIStateType OnUpdate();
 
     protected AIStateMachine stateMachine;
--- Dead Earth/Assets/Dead Earth/Scripts/AI/AIZombieStateMachine.cs
+++ Dead Earth/Assets/Dead Earth/Scripts/AI/AIZombieStateMachine.cs
@@ -2,4 +2,72 @@
 
 public class AIZombieStateMachine : AIStateMachine
 {
+    #region Serialized fields
+
+    [SerializeField] [Range(10.0f, 360.0f)] float fov = 50.0f;
+
+    [SerializeField] [Range(0.0f, 1.0f)] float sight = 0.5f;
+
+    [SerializeField] [Range(0.0f, 1.0f)] float hearing = 50.0f;
+
+    [SerializeField] [Range(0.0f, 1.0f)] float aggression = 0.5f;
+
+    [SerializeField] [Range(0, 100)] int health = 100;
+
+    [SerializeField] [Range(0.0f, 1.0f)] float intelligence = 0.5f;
+
+    [SerializeField] [Range(0.0f, 1.0f)] float satisfaction = 1f;
+
+    #endregion
+
+    #region Public properties
+
+    public float Fov { get => fov; }
+
+    public float Hearing { get => hearing; }
+
+    public float Sight { get => sight; }
+
+    public float Intelligence { get => intelligence; }
+
+    public bool Crawling { get; } = false;
+
+    public bool Feeding { get; set; } = false;
+
+    public int AtackType { get; set; } = 0;
+
+    public int Seeking { get; set; } = 0;
+
+    public float Satisfaction
+    {
+        get => satisfaction;
+        set => satisfaction = value;
+    }
+
+    public float Aggression
+    {
+        get => aggression;
+        set => aggression = value;
+    }
+
+    public int Health
+    {
+        get => health;
+        set => health = value;
+    }
+
+    public float Speed
+    {
+        get
+        {
+            return navAgent != null ? navAgent.speed : 0.0f;
+        }
+        set
+        {
+            if (navAgent != null)
+                navAgent.speed = value;
+        }
+    }
+
+    #endregion
 }
