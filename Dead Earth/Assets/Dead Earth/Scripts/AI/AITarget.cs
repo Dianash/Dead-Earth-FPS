019ae39f3af1aa84e0c3dcaea35612322f841c3a
Dianash <ddiana.shnaider@gmail.com>
2020-04-30 22:34:52 +0300

    Moved AITarget struct to separate file

---
+++ Dead Earth/Assets/Dead Earth/Scripts/AI/AITarget.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Describes a potential target to the AI System
+/// </summary>
+public struct AITarget
+{
+    private AITargetType type;
+    private Collider collider;
+    private Vector3 position;
+    private float distance;
+    private float time;
+
+    public AITargetType Type { get => type; }
+    public Collider Collider { get => collider; }
+    public Vector3 Position { get => position; }
+    public float Time { get => time; }
+
+    public float Distance
+    {
+        get => distance;
+        set => distance = value;
+    }
+
+    /// <summary>
+    /// Sets the target data
+    /// </summary>
+    public void Set(AITargetType type, Collider collider, Vector3 position, float distance)
+    {
+        this.type = type;
+        this.collider = collider;
+        this.position = position;
+        this.distance = distance;
+
+        time = UnityEngine.Time.time;
+    }
+
+    /// <summary>
+    /// Clears the target data
+    /// </summary>
+    public void Clear()
+    {
+        type = AITargetType.None;
+        collider = null;
+        position = Vector3.zero;
+        time = 0.0f;
+        distance = Mathf.Infinity;
+    }
+}
