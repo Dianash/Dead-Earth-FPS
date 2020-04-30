50a2bf343440d9e1a810c26347fa145d818401f2
Dianash <ddiana.shnaider@gmail.com>
2020-05-01 01:08:33 +0300

    Implemented Idle state OnUpdate method

--- Dead Earth/Assets/Dead Earth/Scripts/AI/AIZombieStateIdle1.cs
+++ Dead Earth/Assets/Dead Earth/Scripts/AI/AIZombieStateIdle1.cs
@@ -1,5 +1,8 @@
 ﻿using UnityEngine;
 
+/// <summary>
+/// An AIState that implements a zombie`s Idle behavior
+/// </summary>
 public class AIZombieStateIdle1 : AIZombieState
 {
     [SerializeField] Vector2 idleTimeRange = new Vector2(10.0f, 60.0f);
@@ -7,11 +10,17 @@ public class AIZombieStateIdle1 : AIZombieState
     private float idleTime = 0.0f;
     private float timer = 0.0f;
 
+    /// <returns>
+    /// Returns the type of the state
+    /// </returns>
     public override AIStateType GetStateType()
     {
         return AIStateType.Idle;
     }
 
+    /// <summary>
+    /// Called by the State Machine, when first transitioned into Idle state
+    /// </summary>
     public override void OnEnterState()
     {
         Debug.Log("Entering Idle State");
@@ -28,11 +37,37 @@ public class AIZombieStateIdle1 : AIZombieState
         zombieStateMachine.Speed = 0;
         zombieStateMachine.Seeking = 0;
         zombieStateMachine.Feeding = false;
+        zombieStateMachine.AttackType = 0;
         zombieStateMachine.Clear();
     }
 
     public override AIStateType OnUpdate()
     {
+        if (zombieStateMachine == null) return AIStateType.Idle;
+
+        if (zombieStateMachine.visualThreat.Type == AITargetType.VisualPlayer)
+        {
+            zombieStateMachine.SetTarget(zombieStateMachine.visualThreat);
+            return AIStateType.Pursuit;
+        }
+        if (zombieStateMachine.visualThreat.Type == AITargetType.VisualLight)
+        {
+            zombieStateMachine.SetTarget(zombieStateMachine.visualThreat);
+            return AIStateType.Alerted;
+        }
+        if (zombieStateMachine.audioThreat.Type == AITargetType.Audio)
+        {
+            zombieStateMachine.SetTarget(zombieStateMachine.audioThreat);
+            return AIStateType.Alerted;
+        }
+        if (zombieStateMachine.visualThreat.Type == AITargetType.VisualFood)
+        {
+            zombieStateMachine.SetTarget(zombieStateMachine.visualThreat);
+            return AIStateType.Pursuit;
+        }
+
+        timer += Time.deltaTime;
+        if (timer > idleTime) return AIStateType.Patrol;
 
         return AIStateType.Idle;
     }
