fc127cb83d58bc9608bd5e160d05f465b43edc22
Dianash <ddiana.shnaider@gmail.com>
2020-04-30 22:28:59 +0300

    Created AIZombieStateIdleI class

---
+++ Dead Earth/Assets/Dead Earth/Scripts/AI/AIZombieStateIdle1.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class AIZombieStateIdle1 : AIZombieState
+{
+    public override AIStateType GetStateType()
+    {
+        return AIStateType.Idle;
+    }
+
+    public override AIStateType OnUpdate()
+    {
+
+        return AIStateType.Idle;
+    }
+}
