1a0ecaa273b315db7566077dfeae0f4e1299bab8
Dianash <ddiana.shnaider@gmail.com>
2020-05-05 16:02:05 +0300

    Created AIZombieStateAlerted1 and atached it to AI Entity object

---
+++ Dead Earth/Assets/Dead Earth/Scripts/AI/AIZombieStateAlerted1.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class AIZombieStateAlerted1 : AIZombieState
+{
+    [SerializeField] [Range(1, 60)] float maxDuration = 10.0f;
+
+    public override AIStateType GetStateType()
+    {
+        return AIStateType.Alerted;
+    }
+
+    public override AIStateType OnUpdate()
+    {
+        return AIStateType.Alerted;
+    }
+}
