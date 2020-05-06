daa673a868650c7430ab8a889496074a3c224de3
Dianash <ddiana.shnaider@gmail.com>
2020-05-07 01:15:42 +0300

    Created AIZombieStateFeeding class

---
+++ Dead Earth/Assets/Dead Earth/Scripts/AI/AIZombieStateFeeding.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class AIZombieStateFeeding : AIZombieState
+{
+    [SerializeField] float slerpSpeed = 5.0f;
+
+
+    public override AIStateType GetStateType()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override AIStateType OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
