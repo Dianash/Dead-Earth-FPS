860ab113b61be35e0d8a7c1731f176e0256ff0c8
Diana Shnaider <dshnaider@outlook.com>
2020-05-09 17:02:05 +0300

    Added ParticleSystem reference to game scene manager

--- Dead Earth/Assets/Dead Earth/Scripts/GameSceneManager.cs
+++ Dead Earth/Assets/Dead Earth/Scripts/GameSceneManager.cs
@@ -3,7 +3,14 @@ using System.Collections.Generic;
 
 public class GameSceneManager : MonoBehaviour
 {
+    [SerializeField] ParticleSystem bloodParticle = null;
+
     private static GameSceneManager instance = null;
+
+    public ParticleSystem BloodParticle
+    {
+        get => bloodParticle;
+    }
 
     public static GameSceneManager Instance
     {
