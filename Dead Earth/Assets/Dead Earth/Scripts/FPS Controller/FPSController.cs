e7b94d39188583ff3d881da38cb1c7ee38a72c08
Dianash <ddiana.shnaider@gmail.com>
2020-05-10 02:33:09 +0300

    Added basic serialized fields to FPSController class

--- Dead Earth/Assets/Dead Earth/Scripts/FPS Controller/FPSController.cs
+++ Dead Earth/Assets/Dead Earth/Scripts/FPS Controller/FPSController.cs
@@ -3,5 +3,15 @@
 [RequireComponent(typeof(CharacterController))]
 public class FPSController : MonoBehaviour
 {
+    [SerializeField] private float walkSpeed = 1.0f;
 
+    [SerializeField] private float runSpeed = 4.5f;
+
+    [SerializeField] private float jumpSpeed = 7.5f;
+
+    [SerializeField] private float stickToGroundForce = 5.0f;
+
+    [SerializeField] private float gravityMultyplier = 2.5f;
+
+    [SerializeField] private UnityStandardAssets.Characters.FirstPerson.MouseLook mouseLook;
 }
