15f3944984838a2a4166277a7f3a7f89ff62b90b
Dianash <ddiana.shnaider@gmail.com>
2020-05-10 16:52:10 +0300

    Added movement controll properties

--- Dead Earth/Assets/Dead Earth/Scripts/FPS Controller/FPSController.cs
+++ Dead Earth/Assets/Dead Earth/Scripts/FPS Controller/FPSController.cs
@@ -14,4 +14,24 @@ public class FPSController : MonoBehaviour
     [SerializeField] private float gravityMultyplier = 2.5f;
 
     [SerializeField] private UnityStandardAssets.Characters.FirstPerson.MouseLook mouseLook;
+
+    private Camera camera = null;
+    private Vector3 inputVector = Vector3.zero;
+    private Vector3 moveDirection = Vector3.zero;
+
+    private bool jumpButtonPressed = false;
+    private bool previouslyGrounded = false;
+    private bool isWalking = true;
+    private bool isJumping = false;
+
+    private float fallingTimer = 0.0f;
+
+    private CharacterController characterController = null;
+    private PlayerMoveStatus movementStatus = PlayerMoveStatus.NotMoving;
+
+    public PlayerMoveStatus MovementStatus { get => movementStatus; }
+    public float WalkSpeed { get => walkSpeed; }
+    public float RunSpeed { get => runSpeed; }
+
+
 }
