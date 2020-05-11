7659cf0eaa1e5ef5ccc3785eb230020a3e80e8f0
Dianash <ddiana.shnaider@gmail.com>
2020-05-12 00:58:13 +0300

    Created MeleeZoneTrigger class

---
+++ Dead Earth/Assets/Dead Earth/Scripts/AI/MeleeZoneTrigger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MeleeZoneTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collider)
+    {
+        AIStateMachine machine = GameSceneManager.Instance.GetAIStateMachine(collider.GetInstanceID());
+        if (machine)
+        {
+            machine.InMeleeRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        AIStateMachine machine = GameSceneManager.Instance.GetAIStateMachine(collider.GetInstanceID());
+        if (machine)
+        {
+            machine.InMeleeRange = false;
+        }
+    }
+}
