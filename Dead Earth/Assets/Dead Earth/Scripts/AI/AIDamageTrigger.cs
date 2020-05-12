ea6f9ab19e5d4efe707f17ca28e6a26146a98321
Dianash <ddiana.shnaider@gmail.com>
2020-05-12 21:43:39 +0300

    Set curves value to all atack animations

--- Dead Earth/Assets/Dead Earth/Scripts/AI/AIDamageTrigger.cs
+++ Dead Earth/Assets/Dead Earth/Scripts/AI/AIDamageTrigger.cs
@@ -4,9 +4,26 @@ public class AIDamageTrigger : MonoBehaviour
 {
     [SerializeField] string parameter = null;
 
+    private AIStateMachine stateMachine = null;
+    private Animator animator = null;
+    private int parameterHash = -1;
+
+    private void Start()
+    {
+        stateMachine = transform.root.GetComponentInChildren<AIStateMachine>();
+
+        if (stateMachine != null)
+            animator = stateMachine.Animator;
+
+        parameterHash = Animator.StringToHash(parameter);
+    }
+
     private void OnTriggerStay(Collider collider)
     {
-        if (collider.gameObject.CompareTag("Player"))
+        if (!animator)
+            return;
+
+        if (collider.gameObject.CompareTag("Player") && animator.GetFloat(parameterHash) > 0.9f)
         {
             Debug.Log("Player being damaged");
         }
