0d153d7693a09cd8e5080dd365d7dacb1b31f74f
Dianash <ddiana.shnaider@gmail.com>
2020-05-12 18:30:49 +0300

    Added AIDamageTrigger to zombie`s head and hands

---
+++ Dead Earth/Assets/Dead Earth/Scripts/AI/AIDamageTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class AIDamageTrigger : MonoBehaviour
+{
+    private void OnTriggerStay(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player being damaged");
+        }
+    }
+}
