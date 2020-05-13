c4a5bcdeef40849c0cee13348dbac017d0be71e4
Dianash <ddiana.shnaider@gmail.com>
2020-05-13 17:11:19 +0300

    Created CameraBloodEffect cs script and CameraBloodEffect shader script

---
+++ Dead Earth/Assets/Dead Earth/Scripts/ImageEffects/CameraBloodEffect.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CameraBloodEffect : MonoBehaviour
+{
+    [SerializeField] private Shader shader = null;
+
+    private Material material = null;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (shader == null) return;
+
+        if (material == null)
+        {
+            material = new Material(shader);
+        }
+        if (material == null) return;
+
+        Graphics.Blit(source, destination, material);
+    }
+}
