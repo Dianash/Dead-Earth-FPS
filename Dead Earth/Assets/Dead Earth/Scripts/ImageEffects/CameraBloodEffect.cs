5b1c598fca4392207e43f3185df5b85b0c9453d4
Dianash <ddiana.shnaider@gmail.com>
2020-05-14 13:53:43 +0300

    Set data that has to be sent to the shader

--- Dead Earth/Assets/Dead Earth/Scripts/ImageEffects/CameraBloodEffect.cs
+++ Dead Earth/Assets/Dead Earth/Scripts/ImageEffects/CameraBloodEffect.cs
@@ -3,19 +3,51 @@
 [ExecuteInEditMode]
 public class CameraBloodEffect : MonoBehaviour
 {
+    [SerializeField] private Texture2D bloodTexture = null;
+    [SerializeField] private Texture2D bloodNormalMap = null;
+
+    [SerializeField] private float bloodAmount = 0.0f;
+    [SerializeField] private float minBloodAmount = 0.0f;
+    [SerializeField] private float distortion = 1.0f;
+    [SerializeField] private float fadeSpeed = 0.05f;
+
+    [SerializeField] private bool autoFade = true;
+
     [SerializeField] private Shader shader = null;
 
     private Material material = null;
 
+    public float BloodAmount { get => bloodAmount; set => bloodAmount = value; }
+    public float MinBloodAmount { get => minBloodAmount; set => minBloodAmount = value; }
+    public float FadeSpeed { get => fadeSpeed; set => fadeSpeed = value; }
+    public bool AutoFade { get => autoFade; set => autoFade = value; }
+
+    private void Update()
+    {
+        if (autoFade)
+        {
+            bloodAmount -= fadeSpeed * Time.deltaTime;
+            bloodAmount = Mathf.Max(bloodAmount, minBloodAmount);
+        }
+    }
+
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
         if (shader == null) return;
-
         if (material == null)
         {
             material = new Material(shader);
         }
         if (material == null) return;
+
+        if (bloodTexture != null)
+            material.SetTexture("_BloodTex", bloodTexture);
+
+        if (bloodNormalMap != null)
+            material.SetTexture("_BloodBump", bloodNormalMap);
+
+        material.SetFloat("Distortion", distortion);
+        material.SetFloat("_BloodAmount", bloodAmount);
 
         Graphics.Blit(source, destination, material);
     }
--- Dead Earth/Assets/Dead Earth/Shaders/CameraBloodEffect.shader
+++ Dead Earth/Assets/Dead Earth/Shaders/CameraBloodEffect.shader
@@ -47,7 +47,6 @@
 			float _BloodAmount;
 			float _BloodDistortion;
 
-
             fixed4 frag (v2f i) : SV_Target
             {
                 fixed4 col = tex2D(_MainTex, i.uv);
