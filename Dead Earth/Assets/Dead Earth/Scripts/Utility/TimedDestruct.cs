3c04047f54a63765a18c3dfd2079e49a5fb92605
Dianash <ddiana.shnaider@gmail.com>
2020-06-04 20:23:05 +0300

    Added new zombie entity to scene

---
+++ Dead Earth/Assets/Dead Earth/Scripts/Utility/TimedDestruct.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedDestruct : MonoBehaviour
+{
+    [SerializeField] private float time = 10.0f;
+
+    private void Awake()
+    {
+        Invoke("DestroyNow", time);
+    }
+
+    private void DestroyNow()
+    {
+        Destroy(gameObject);
+    }
+}
