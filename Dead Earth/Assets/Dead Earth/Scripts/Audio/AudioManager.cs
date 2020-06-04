c246d4de953c0986bdcd0267d48732396e8c8d08
Dianash <ddiana.shnaider@gmail.com>
2020-06-05 01:16:27 +0300

    Created Audio Manager class

---
+++ Dead Earth/Assets/Dead Earth/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioMixer mixer = null;
+
+    private static AudioManager instance = null;
+    private Dictionary<string, TrackInfo> tracks = new Dictionary<string, TrackInfo>();
+
+    public static AudioManager Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = (AudioManager)FindObjectOfType(typeof(AudioManager));
+
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        if (!mixer) return;
+
+        AudioMixerGroup[] groups = mixer.FindMatchingGroups(string.Empty);
+
+        foreach (AudioMixerGroup group in groups)
+        {
+            TrackInfo trackInfo = new TrackInfo();
+            trackInfo.group = group;
+            trackInfo.trackFader = null;
+            tracks[group.name] = trackInfo;
+        }
+    }
+}
