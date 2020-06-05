e614deb2fef61d7ecb4e4102ef96dc9431fb863a
Dianash <ddiana.shnaider@gmail.com>
2020-06-05 19:24:02 +0300

    Created functionality to fetch random audio clips

--- Dead Earth/Assets/Dead Earth/Scripts/Scriptible Objects/AudioCollection.cs
+++ Dead Earth/Assets/Dead Earth/Scripts/Scriptible Objects/AudioCollection.cs
@@ -1,6 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+
+
+public class ClipBank
+{
+    public List<AudioClip> clips = new List<AudioClip>();
+}
+
 
 [CreateAssetMenu(fileName = "New Audio Collection")]
 public class AudioCollection : ScriptableObject
@@ -9,4 +15,50 @@ public class AudioCollection : ScriptableObject
     [SerializeField] [Range(0.0f, 1.0f)] float volume = 1.0f;
     [SerializeField] [Range(0.0f, 1.0f)] float spatialBlend = 1.0f;
     [SerializeField] [Range(0, 256)] int priority = 128;
+    [SerializeField] List<ClipBank> audioClipBanks = new List<ClipBank>();
+
+    public string AudioGroup { get => audioGroup; }
+    public float Volume { get => volume; }
+    public float SpatialBlend { get => spatialBlend; }
+    public int Priority { get => priority; }
+    public int BankCount { get => audioClipBanks.Count; }
+
+    /// <summary>
+    /// Fetches a random audio clip from the bank
+    /// </summary>
+    public AudioClip this[int i]
+    {
+        get
+        {
+            if (audioClipBanks == null || audioClipBanks.Count <= i)
+                return null;
+
+            if (audioClipBanks[i].clips.Count == 0)
+                return null;
+
+            // Fetch the ClipBank to sample from
+            List<AudioClip> clipList = audioClipBanks[i].clips;
+
+            // Select random clip from the bank
+            AudioClip clip = clipList[Random.Range(0, clipList.Count)];
+
+            return clip;
+        }
+    }
+
+    /// <summary>
+    /// Returns a random audio clip from the 1st clip bank;
+    /// </summary>
+    public AudioClip AudioClip
+    {
+        get
+        {
+            if (audioClipBanks == null || audioClipBanks.Count == 0) return null;
+            if (audioClipBanks[0].clips.Count == 0) return null;
+
+            List<AudioClip> clipList = audioClipBanks[0].clips;
+            AudioClip clip = clipList[Random.Range(0, clipList.Count)];
+            return clip;
+        }
+    }
 }
