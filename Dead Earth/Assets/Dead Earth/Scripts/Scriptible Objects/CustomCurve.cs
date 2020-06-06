81a3b0a9f73b6c98500b25c9a7e7eec3b8a58c52
Dianash <ddiana.shnaider@gmail.com>
2020-06-06 17:27:45 +0300

    Addded custom curves to animations

---
+++ Dead Earth/Assets/Dead Earth/Scripts/Scriptible Objects/CustomCurve.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Custom Animation Curve")]
+public class CustomCurve : ScriptableObject
+{
+    [SerializeField] AnimationCurve _curve = new AnimationCurve(new Keyframe(0, 0), new Keyframe(1, 0));
+
+    public float Evaluate(float time)
+    {
+        return _curve.Evaluate(time);
+    }
+}
