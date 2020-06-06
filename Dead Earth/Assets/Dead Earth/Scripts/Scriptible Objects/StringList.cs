a56b65933d47714c3eac5648f042ae86e16ea385
Dianash <ddiana.shnaider@gmail.com>
2020-06-06 17:51:03 +0300

    Created custom layer exclusion collection

---
+++ Dead Earth/Assets/Dead Earth/Scripts/Scriptible Objects/StringList.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New string list")]
+public class StringList : ScriptableObject
+{
+    [SerializeField] List<string> stringList = new List<string>();
+
+    public string this[int i]
+    {
+        get
+        {
+            if (i < stringList.Count)
+            {
+                return stringList[i];
+            }
+            return null;
+        }
+    }
+}
