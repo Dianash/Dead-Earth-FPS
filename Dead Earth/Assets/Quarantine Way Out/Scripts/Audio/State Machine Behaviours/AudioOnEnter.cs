b058a998553f91716e8b7a9170cfc1b9191eaf04
Dianash <ddiana.shnaider@gmail.com>
2020-06-08 02:38:08 +0300

    Created generatir button animator controller and set up audio collections to anim, states

---
+++ Dead Earth/Assets/Quarantine Way Out/Scripts/Audio/State Machine Behaviours/AudioOnEnter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class AudioOnEnter : StateMachineBehaviour
+{
+    [SerializeField] AudioCollection audioCollection = null;
+    [SerializeField] int bank = 0;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (AudioManager.Instance == null || audioCollection == null) 
+            return;
+
+        AudioManager.Instance.PlayOneShotSound(audioCollection.AudioGroup, audioCollection[bank], animator.transform.position,
+            audioCollection.Volume, audioCollection.SpatialBlend, audioCollection.Priority);
+    }
+}
