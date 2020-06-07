6decff1cd5584ebe40d3165d464760d8c5b8f865
Dianash <ddiana.shnaider@gmail.com>
2020-06-07 23:49:56 +0300

    Created keypad gameobject

---
+++ Dead Earth/Assets/Quarantine Way Out/Scripts/Interective Items/InteractiveKeypad.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveKeypad : InteractiveItem
+{
+	[SerializeField] protected Transform elevator = null;
+	[SerializeField] protected AudioCollection collection = null;
+	[SerializeField] protected int bank = 0;
+	[SerializeField] protected float activationDelay = 0.0f;
+
+	bool isActivated = false;
+}
