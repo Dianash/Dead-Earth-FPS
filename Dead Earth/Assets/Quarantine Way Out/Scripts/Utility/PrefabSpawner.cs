c1a9860996de4b06139f9746f7a148979d900080
Dianash <ddiana.shnaider@gmail.com>
2020-06-08 21:49:57 +0300

    Added access code spawning

---
+++ Dead Earth/Assets/Quarantine Way Out/Scripts/Utility/PrefabSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject prefab = null;
+    [SerializeField] private List<Transform> spawnPoints = new List<Transform>();
+
+    private void Awake()
+    {
+        if (spawnPoints.Count == 0 || prefab == null) 
+            return;
+
+        Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Count)];
+        Instantiate(prefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
