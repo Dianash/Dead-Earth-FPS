59cea8450e7474322e9a6a2cb1538db9a9bc75ca
Dianash <ddiana.shnaider@gmail.com>
2020-06-09 00:42:18 +0300

    Made skybox rotation

---
+++ Dead Earth/Assets/Quarantine Way Out/Scripts/Utility/RotatingSky.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class RotatingSky : MonoBehaviour
+{
+    // Inspector Assigned
+    [SerializeField] protected Material skyMaterial = null;
+    [SerializeField] protected float speed = 1.0f;
+
+    // Internals
+    protected float angle = 0;
+    protected float originalAngle = 0;
+
+    void OnEnable()
+    {
+        if (skyMaterial)
+            originalAngle = angle = skyMaterial.GetFloat("_Rotation");
+    }
+
+
+    void OnDisable()
+    {
+        if (skyMaterial)
+            skyMaterial.SetFloat("_Rotation", originalAngle);
+    }
+
+    void Update()
+    {
+        if (skyMaterial == null)
+            return;
+
+        angle += speed * Time.deltaTime;
+
+        if (angle > 360.0f)
+        {
+            angle -= 360.0f;
+        }
+        else if (angle < 0.0f)
+        {
+            angle += 360.0f;
+        }
+
+        skyMaterial.SetFloat("_Rotation", angle);
+    }
+}
